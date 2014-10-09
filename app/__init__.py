9b69a740cccf346445d20f1bfdd84c5d83878797
defshine <crazyxin1988@163.com>
2014-10-09 11:29:07 +0800

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from flask.ext.login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config')
+# for gunicorn
+from werkzeug.contrib.fixers import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
