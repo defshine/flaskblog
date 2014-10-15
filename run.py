3ee5b58bce831e32aec9413cb2dd6427f47bc578
defshine <crazyxin1988@163.com>
2014-10-15 12:01:24 +0800

    fix

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from app import create_app
 
 app = create_app()
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+app.run(host='0.0.0.0')
--- wsgi_gunicorn.py
+++ wsgi_gunicorn.py
@@ -8,5 +8,5 @@ app = create_app()
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+app.run(host='0.0.0.0')
 
