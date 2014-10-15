c0b56dd6e5b8d73709e506f5b5d17ea35be1b501
defshine <crazyxin1988@163.com>
2014-10-15 11:56:44 +0800

    add gunicorn

--- run.py
+++ run.py
@@ -5,6 +5,4 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == "main":
-
-    app.run(host='0.0.0.0', port=5000, Debug=True)
+app.run(host='0.0.0.0', port=5000, debug=True)
--- wsgi_gunicorn.py
+++ wsgi_gunicorn.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from werkzeug.serving import run_simple
 from werkzeug.contrib.fixers import ProxyFix
 from app import create_app
 
@@ -9,6 +8,5 @@ app = create_app()
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, Debug=True)
+app.run(host='0.0.0.0', port=5000, debug=True)
 
