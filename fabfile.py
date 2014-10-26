d1590909271a31b7037ea4c7518fa7bc154ef068
defshine <crazyxin1988@163.com>
2014-10-26 18:35:37 +0800

    change

--- fabfile.py
+++ fabfile.py
@@ -6,30 +6,29 @@ env.user = ''
 env.password = ''
 
 
-def add():
-    local("git add .")
-
-
-def commit():
-    local("git commit -a ")
-
-
-def push():
-    local("git push")
-
-
-def pull():
-    local("git pull")
-
-
+#clone code from github
 def clone():
     code_dir = '/home/xin/www'
     with cd(code_dir):
         run("git clone https://github.com/defshine/flaskblog.git")
 
 
-def deploy():
+#update project
+def update():
     code_dir = '/home/xin/www/flaskblog'
     with cd(code_dir):
         run("git pull")
+
+
+#restart project
+def restart():
+    sudo("supervisorctl restart flaskblog")
+    sudo("supervisorctl status")
+
+
+#check the project status
+def check():
+    sudo("supervisorctl status")
+
+
 
